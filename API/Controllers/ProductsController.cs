00a60d65a89c046deeb819cfa559d833cb950ed3
DariuszKitel <dariuszkitel@gmail.com>
2022-07-21 01:09:03 +0200

    Rebuild project for better infrastructure and add entry functionality for product

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ECommerceDbContext _dbContext;
+
+        public ProductsController(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Infrastructure/Data/ECommerceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
