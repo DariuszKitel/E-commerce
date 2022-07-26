809f9150672ad6068d61f25ca9339fa67ddbd580
DariuszKitel <dariuszkitel@gmail.com>
2022-07-26 15:16:37 +0200

    Created UI client on angular project with start settings like bootstrap, font-awesome, routing, ssl and navbar.

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,7 +42,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:44355");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
 
             });
